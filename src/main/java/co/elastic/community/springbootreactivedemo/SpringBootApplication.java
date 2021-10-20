bfcf832af2135647bea41f5c6a29c927f3dc7477
Alexander Reelsen <alexander@reelsen.net>
2021-10-20 17:18:21 +0200

    Add Elastic APM agent

--- src/main/java/co/elastic/community/springbootreactivedemo/SpringBootApplication.java
+++ src/main/java/co/elastic/community/springbootreactivedemo/SpringBootApplication.java
@@ -1,11 +1,13 @@
 package co.elastic.community.springbootreactivedemo;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootApplication {
 
 	public static void main(String[] args) {
+		ElasticApmAttacher.attach();
 		SpringApplication.run(SpringBootApplication.class, args);
 	}
 
