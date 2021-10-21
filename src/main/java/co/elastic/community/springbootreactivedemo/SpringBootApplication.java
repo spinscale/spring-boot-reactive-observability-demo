3f4086fee0e61798a5e94ed415909c671685df1a
Alexander Reelsen <alexander@reelsen.net>
2021-10-21 10:57:10 +0200

    Add traceable background task

--- src/main/java/co/elastic/community/springbootreactivedemo/SpringBootApplication.java
+++ src/main/java/co/elastic/community/springbootreactivedemo/SpringBootApplication.java
@@ -2,8 +2,10 @@ package co.elastic.community.springbootreactivedemo;
 
 import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
+@EnableScheduling
 public class SpringBootApplication {
 
 	public static void main(String[] args) {
